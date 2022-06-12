da15af1cab0f818c553f8fa1bfec745f3c0db456
mdhUser <734570993@qq.com>
2022-06-12 16:17:49 +0800

    update

---
+++ src/main/java/cn/itcast/hotel/constant/MqConstants.java
@@ -0,0 +1,21 @@
+package cn.itcast.hotel.constant;
+
+/**
+ * @description:
+ * @author: Maxwell
+ * @email: maodihui@foxmail.com
+ * @date: 2022/6/7 0:38
+ */
+public class MqConstants {
+
+    /**
+     * 监听新增和修改的队列
+     */
+    public final static String HOTEL_INSERT_QUEUE = "hotel.insert.queue";
+    /**
+     * 监听删除的队列
+     */
+    public final static String HOTEL_DELETE_QUEUE = "hotel.delete.queue";
+
+
+}
